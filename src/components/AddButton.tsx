3ee7028159b825052289d4a8b43f26f09675a242
Ankit <ankitpurohit@softcolon.com>
2022-08-02 17:48:05 +0530

    vercel url added

--- src/components/AddButton.tsx
+++ src/components/AddButton.tsx
@@ -1,6 +1,186 @@
-import React from "react";
+import React, { useState } from "react";
+
+// toastify
+import { toast } from "react-toastify";
+
+// data
+import config from "../config";
+
+// web3
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  LAMPORTS_PER_SOL,
+  Transaction,
+  PublicKey,
+  SystemProgram,
+  Keypair,
+} from "@solana/web3.js";
+
+// axios
+const axios = require("axios");
 
 const AddButton = () => {
+  //public key and connection
+  const { publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
+  const accountAddress = String(publicKey);
+
+  const [amount, setAmount] = useState("0");
+
+  const addBalance = async () => {
+    if (!publicKey) {
+      return toast.error("Please Connect Your Wallet", {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+      });
+    } else {
+      const userSOLBalance =
+        (await connection.getBalance(publicKey)) / LAMPORTS_PER_SOL;
+
+      if (userSOLBalance > Number(amount)) {
+        try {
+          // const TRANSFER_SOL_AMOUNT =
+          //   parseFloat((values[0] / 100).toFixed(2)) * LAMPORTS_PER_SOL;
+
+          if (Number(amount) === 0) {
+            toast.error("0 Sol not allowd!");
+          } else {
+            toast.loading(
+              "Transaction is detected.\n Please do not reload page."
+            );
+
+            const transaction = new Transaction().add(
+              SystemProgram.transfer({
+                fromPubkey: publicKey,
+                toPubkey: new PublicKey(config.SOL_RECIVER_ADDRESS),
+                lamports: Number(amount),
+              })
+            );
+
+            console.log(transaction);
+
+            const signature = await SendTransactionFunction(
+              connection,
+              transaction,
+              []
+            );
+
+            console.log(signature);
+
+            // const hash = await signature.hash;
+
+            setTimeout(async () => {
+              const myHeaders = new Headers();
+              myHeaders.append("Content-Type", "application/json");
+
+              const raw = JSON.stringify({
+                user: "98",
+              });
+
+              fetch(`${config.API}/api/transaction/addBalance`, {
+                method: "POST",
+                headers: myHeaders,
+                body: raw,
+                redirect: "follow",
+              })
+                .then((response) => response.text())
+                .then((result) => console.log(result))
+                .catch((error) => {
+                  console.log(error);
+
+                  let msg = (error as Error).message;
+                  console.log(msg);
+
+                  if (msg === "User rejected the request.") {
+                    toast.error("You cancel transaction request");
+                    setTimeout(() => {
+                      window.location.reload();
+                    }, 3000);
+                  } else {
+                    toast.error("Something went Wrong", {
+                      position: toast.POSITION.TOP_LEFT,
+                    });
+                  }
+                });
+
+              // const result = await axios.post(
+              //   config.API + "/api/transaction/addBalance",
+              //   {
+              //     user: publicKey,
+              //     transactionAmount: Number(amount),
+              //     // transactionHas: hash,
+              //   }
+              // );
+
+              // const data = await result.data;
+
+              // console.log(data);
+            }, 3000);
+          }
+        } catch (error) {
+          console.log(error);
+
+          let msg = (error as Error).message;
+          console.log(msg);
+
+          if (msg === "User rejected the request.") {
+            toast.error("You cancel transaction request");
+            setTimeout(() => {
+              window.location.reload();
+            }, 3000);
+          } else {
+            toast.error("Something went Wrong", {
+              position: toast.POSITION.TOP_LEFT,
+            });
+          }
+        }
+      } else {
+        toast.error(
+          `required minimum ${amount} SOL. You have right now ${userSOLBalance} SOL`
+        );
+      }
+    }
+  };
+
+  async function SendTransactionFunction(
+    conn: any,
+    transaction: any,
+    signers: Keypair[]
+  ) {
+    // if (window.solana.publicKey) {
+    //   transaction.feePayer = window.solana.publicKey;
+    //   transaction.recentBlockhash = (
+    //     await conn.getRecentBlockhash("max")
+    //   ).blockhash;
+    //   await transaction.setSigners(
+    //     window.solana.publicKey,
+    //     ...signers.map((s) => s.publicKey)
+    //   );
+    //   if (signers.length != 0) await transaction.partialSign(...signers);
+    //   if (window.solana) {
+    //     const signedTransaction = await window.solana.signTransaction(
+    //       transaction
+    //     );
+    //     let hash = await conn.sendRawTransaction(
+    //       await signedTransaction.serialize()
+    //     );
+    //     let statusCode = 201;
+    //     const confiermationTransaction = JSON.parse(
+    //       JSON.stringify(await conn.confirmTransaction(hash))
+    //     );
+    //     if (confiermationTransaction) {
+    //       statusCode = 200;
+    //     }
+    //     return { status: statusCode, hash: hash };
+    //   }
+    // }
+  }
+
   return (
     <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
       <input
@@ -8,10 +188,15 @@ const AddButton = () => {
         id="email"
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         required
+        value={amount}
+        onChange={(e) => {
+          setAmount(e.target.value);
+        }}
       />
       <button
         type="button"
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 mt-3"
+        onClick={addBalance}
       >
         Add Balance
       </button>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,20 +1,42 @@
+// react
 import React, { useEffect, useState } from "react";
+
+// data
 import config from "../config";
+
+// components
 import AddButton from "../components/AddButton";
 import Navbar from "../components/Navbar";
 import ShowBalance from "../components/ShowBalance";
 import WithDrawButton from "../components/WithDrawButton";
-import { ToastContainer, toast } from "react-toastify";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 
+// toastify
+import { ToastContainer, toast } from "react-toastify";
+
+// web3
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  LAMPORTS_PER_SOL,
+  Transaction,
+  PublicKey,
+  SystemProgram,
+  Keypair,
+} from "@solana/web3.js";
+
+// axios
 const axios = require("axios");
 
 const HomePage = () => {
+  // set Coin and Amount
   const [coin, setCoin] = useState(0);
-  const { publicKey } = useWallet();
+  const [amount, setAmount] = useState(0.05);
 
+  //public key and connection
+  const { publicKey, sendTransaction } = useWallet();
+  const { connection } = useConnection();
   const accountAddress = String(publicKey);
 
+  // user address and balance
   const [user, setUser] = useState("");
   const [balance, setBalance] = useState(0);
 
@@ -22,6 +44,7 @@ const HomePage = () => {
     findAddress();
   }, [publicKey]);
 
+  // find user
   const findAddress = async () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -53,6 +76,7 @@ const HomePage = () => {
       });
   };
 
+  // sign in
   const signInUser = async () => {
     // if user found
     var myHeaders = new Headers();
@@ -76,6 +100,7 @@ const HomePage = () => {
       .catch((error) => console.log("error", error));
   };
 
+  // sign up
   const signUpUser = () => {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
@@ -98,6 +123,7 @@ const HomePage = () => {
       .catch((error) => console.log("error", error));
   };
 
+  // show result
   const showResult = () => {
     if (publicKey === null) {
       return toast.error("Please Connect Your Wallet", {
@@ -110,43 +136,35 @@ const HomePage = () => {
         progress: undefined,
       });
     }
-    if (coin === 0) {
-      toast.error("Select Head or Tail First", {
+
+    const randomNumber = Math.floor(Math.random() * 2);
+    if (coin === randomNumber) {
+      toast.success(`Congrats You win ${amount * 2} sol`, {
         position: "top-right",
-        autoClose: 1000,
-        hideProgressBar: true,
+        autoClose: 5000,
+        hideProgressBar: false,
         closeOnClick: true,
         pauseOnHover: true,
         draggable: true,
         progress: undefined,
       });
     } else {
-      const randomNumber = Math.floor(Math.random() * 2) + 1;
-      if (coin === randomNumber) {
-        toast.success("Congrats You win this game", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      } else {
-        toast.error("Sorry! Better Luck Next Time", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
-
-      setCoin(0);
+      toast.error("Sorry! Better Luck Next Time", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
     }
+
+    setCoin(0);
   };
+
+  // send transaction
+  const sendSol = () => {};
 
   return (
     <div>
@@ -168,39 +186,112 @@ const HomePage = () => {
         </div>
       </section>
 
-      <div className="p-7 bg-slate-300">
-        <section className="text-gray-600 body-font">
-          <div className="container px-5  mx-auto">
-            <div className="flex flex-wrap -m-4 text-center">
-              <div className="p-4 md:w-1/6 sm:w-1/3 w-full">
-                <div
-                  className={`bg-yellow-400 py-3 ${
-                    coin === 1 && "border border-black"
-                  } cursor-pointer`}
-                  onClick={() => {
-                    setCoin(1);
-                  }}
-                >
-                  Head
-                </div>
-              </div>
-              <div className="p-4 md:w-1/6 sm:w-1/3 w-full">
-                <div
-                  className={`bg-yellow-400 py-3 ${
-                    coin === 2 && "border border-black"
-                  } cursor-pointer`}
-                  onClick={() => {
-                    setCoin(2);
-                  }}
-                >
-                  Tail
-                </div>
-              </div>
+      <div className="flex flex-col items-center bg-slate-300">
+        <section className="">
+          <div className="text-center my-4">I Like</div>
+          <div className="flex gap-3">
+            <div
+              className={`bg-yellow-400 w-[100px] h-[50px] flex items-center justify-center ${
+                coin === 0 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setCoin(0);
+              }}
+            >
+              Head
+            </div>
+
+            <div
+              className={`bg-yellow-400 w-[100px] h-[50px] flex items-center justify-center ${
+                coin === 1 && "border-2 border-black"
+              } cursor-pointer`}
+              onClick={() => {
+                setCoin(1);
+              }}
+            >
+              Tail
             </div>
           </div>
         </section>
 
-        <div className="mt-10 ml-24">
+        <section className="">
+          <div className="text-center my-4">For</div>
+          <div className="grid grid-cols-3 gap-3">
+            {/* 0.05 */}
+            <div
+              className={`bg-yellow-400 w-[300px] h-[50px] flex justify-center items-center ${
+                amount === 0.05 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(0.05);
+              }}
+            >
+              0.05 sol
+            </div>
+
+            {/* 0.1 */}
+            <div
+              className={`bg-yellow-400 w-[300px] h-[50px] flex justify-center items-center ${
+                amount === 0.1 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(0.1);
+              }}
+            >
+              0.1 sol
+            </div>
+
+            {/* 0.25 */}
+            <div
+              className={`bg-yellow-400 w-[300px] h-[50px] flex justify-center items-center ${
+                amount === 0.25 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(0.25);
+              }}
+            >
+              0.25 sol
+            </div>
+
+            {/* 0.5  */}
+            <div
+              className={`bg-yellow-400 w-[300px] h-[50px] flex justify-center items-center ${
+                amount === 0.5 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(0.5);
+              }}
+            >
+              0.5 sol
+            </div>
+
+            {/* 1  */}
+            <div
+              className={`bg-yellow-400 w-[300px]  h-[50px] flex justify-center items-center ${
+                amount === 1 && " border-black border-2 "
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(1);
+              }}
+            >
+              1 sol
+            </div>
+
+            {/* 2  */}
+            <div
+              className={`bg-yellow-400 w-[300px] h-[50px] flex justify-center items-center ${
+                amount === 2 && " border-black border-2"
+              } cursor-pointer`}
+              onClick={() => {
+                setAmount(2);
+              }}
+            >
+              2 sol
+            </div>
+          </div>
+        </section>
+
+        <div className="my-10">
           <button
             type="button"
             className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full px-5 py-2.5 text-center mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
